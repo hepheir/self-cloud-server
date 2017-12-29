65854c37b7ffe231418057a5cee15fb21db9d1a6
Hepheir <hepheir@gmail.com>
2017-12-30 04:07:11 +0900

    log.js update

--- modules/dist/log.js
+++ modules/dist/log.js
@@ -1,53 +1,84 @@
 'use strict';
 
+var date = new Date();
+
 const fs = require('fs');
 
-var LASTEST_LOG = '';
 
-// Path
-
-var LOG_PATH;
-
-// Module
-
-module.exports.setLogPath = setLogPath; // Initializer
-
-module.exports.create = create;
-
-
-// FUNCTIONS
-
-function setLogPath(path) {
-    let date = new Date();
-    LOG_PATH = path;
+function _log(LOG_PATH)
+{
+    let LASTEST_LOG = '',
+        L_LOG_TIME = 0;
     
-    if (!fs.existsSync(path)) {
-        // Create a new log file.
-        fs.closeSync(fs.openSync(path, 'w'));
-
-        console.log(`[${path}] created.`);
-        fs.writeFileSync(path, `[${date.toLocaleString()}] [${path}] created.`);
+    // 1. Get the lastest log message.
+    function _lastest() {
+        return LASTEST_LOG;
     }
+    
+    // 2. Create a new log file.
+    function _new()
+    {
+        // YYYYMMDD
+        let file = `${date.getFullYear()}${date.getMonth() + 1}${date.getDate()}`;
+
+        let path = LOG_PATH + file;
+
+        if (!fs.existsSync(path))
+        {
+            let CUR_TIME = date.toTimeString();
+
+            fs.closeSync(fs.openSync(path, 'a'));
+            console.log(`[${path}] created.`);
+    
+            fs.writeFileSync(path, `[${CUR_TIME}]\n\t[${path}] was created.`);
+            L_LOG_TIME = CUR_TIME;
+        }
+
+        return path;
+    }
+
+    // 3. Log a new message.
+    function _create(msg) {
+        // 3-1. One log file for each dates.
+        let path = _new();
+
+        let message;
+
+        // 3-2. Use '*' for the duplicated messages.
+        if (msg === LASTEST_LOG) {
+            message = '*';
+        }
+        // 3-3. Regular log.
+        else
+        {
+            message = '\n';
+
+            console.log(msg);
+            LASTEST_LOG = msg;
+
+            let CUR_TIME = date.toTimeString();
+
+            // 3-3-1. If multiple logs registered in a second, skip time recording.
+            if (L_LOG_TIME !== CUR_TIME)
+            {
+                message += `[${CUR_TIME}]\n`;
+                L_LOG_TIME = CUR_TIME;
+            }
+
+            message += `\t${msg} `;
+        }
+
+        // 3-3. Record.
+        fs.appendFileSync(path, message, 'utf-8');
+    }
+
+
+    return {
+        lastest: _lastest,
+        create: _create
+    };
 }
 
-/**
- * Create a log record of the message.
- * @param {String} message 
- */
-function create(message) {
-    let history  = fs.readFileSync(LOG_PATH),
-        addition;
+// ######## MODULE EXPORTS ######## //
 
-    if (LASTEST_LOG === message) {
-        addition = '*';
-
-    } else {
-        let date = new Date();
-        addition = `\n[${date.toLocaleString()}] ${message} `;
-
-        console.log(message);
-        LASTEST_LOG = message;
-    }
-
-    fs.writeFileSync(LOG_PATH, history + addition);
-}
+module.exports = _log;
