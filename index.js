b10f3bf9b43979d82fbb0dc37c31be4b959d6130
Hepheir <hepheir@gmail.com>
2018-09-22 03:36:44 +0900

    Start buliding alpha-0.0

--- index.js
+++ index.js
@@ -1,8 +1,57 @@
 #!/usr/bin/env node
 
+const PORT = 80;
+
+// ################################### //
+
 'use strict';
 
-const boot = require('boot');
+const fs = require('fs');
+const express = require('express');
 
-const fs = require('fs')
-    , http = require('http');
+// ################################### //
+
+const app = app_create();
+
+function _main_() {
+    app.get(/.*/,  router_renderUI);
+    app.post(/.*/, router_readDir);
+
+    app_listen();
+}
+
+// ################################### //
+
+function app_create() {
+    let app;
+    
+    app = express();
+    app.use(express.static('app'));
+
+    return app;
+}
+
+function app_listen() {
+    app.listen(PORT, () => {
+        console.log(`Self-cloud-server listening on [${PORT}]!`);
+    });
+}
+
+// ################################### //
+
+function router_renderUI(req, res) {
+    // Not Prepared Yet.
+}
+
+function router_readDir(req, res) {
+    let content;
+
+    content = fs.readdirSync('G:/Database/');
+    content = JSON.stringify(content);
+
+    res.send(content);
+}
+
+// ################################### //
+
+_main_();
